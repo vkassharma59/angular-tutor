c1f10c2768e21fb1bd53b9803daa32b2f558c215
vikas sharma <vikas.sharma59@gamil.com>
2022-11-18 00:07:11 +0530

    worked on custom dirctive with event

--- src/app/basic-heighlight-directive/basic-heighlight.directive.ts
+++ src/app/basic-heighlight-directive/basic-heighlight.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[addBasicHighlight]'
@@ -18,5 +18,14 @@ export class BasicHighlightText implements OnInit {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#e7e7e7');
     this.renderer.setStyle(this.elementRef.nativeElement, 'border-radius', '5px');
     this.renderer.setStyle(this.elementRef.nativeElement, 'border', '2px solid #ddd');
+  }
+
+  //@HostListerner('event name') function name can be anything
+  @HostListener('mouseover') mouseover(eventData: Event){
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#ccc');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event){
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#e7e7e7');
   }
 }
