e17551da2446eaf5a403e331ed70c76d1a49cf99
vikas sharma <vikas.sharma59@gamil.com>
2022-11-17 23:37:46 +0530

    created custom directive

--- src/app/basic-heighlight-directive/basic-heighlight.directive.ts
+++ src/app/basic-heighlight-directive/basic-heighlight.directive.ts
@@ -1,17 +1,22 @@
-import { Directive, OnInit, ElementRef } from '@angular/core';
+import { Directive, OnInit, ElementRef, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[addBasicHighlight]'
 })
 
 export class BasicHighlightText implements OnInit {
-  constructor(private elementRef: ElementRef){}
+  constructor(private elementRef: ElementRef, private renderer: Renderer2){}
 
   ngOnInit(): void {
+
     this.elementRef.nativeElement.style.backgroundColor = 'blue';
-    this.elementRef.nativeElement.style.color = '#fff';
-    this.elementRef.nativeElement.style.paddingTop = '20px';
-    this.elementRef.nativeElement.style.paddingBottom = '20px';
-    this.elementRef.nativeElement.style.paddingLeft = '20px';
+    this.elementRef.nativeElement.style.color = '#777';
+    this.elementRef.nativeElement.style.paddingTop = '15px';
+    this.elementRef.nativeElement.style.paddingBottom = '15px';
+    this.elementRef.nativeElement.style.paddingLeft = '15px';
+
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', '#e7e7e7');
+    this.renderer.setStyle(this.elementRef.nativeElement, 'border-radius', '5px');
+    this.renderer.setStyle(this.elementRef.nativeElement, 'border', '2px solid #ddd');
   }
 }
